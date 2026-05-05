c955b10e80bb4b927fae7351c9befe0fde47963f
YoungBaller555 <149848045+YoungBaller555@users.noreply.github.com>
2026-05-05 16:56:40 -0400

    Ajout_musique de fon + correction de son porte + correction potions et ajustements volumes

---
+++ tp3_nathan_vasile_xavier/Assets/scripts/sondefond.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class MusiqueFondScene : MonoBehaviour
+{
+    public AudioClip musiquefondscene01;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        audioSource.clip = musiquefondscene01;
+        audioSource.loop = true;
+        audioSource.playOnAwake = false;
+        audioSource.spatialBlend = 0f; // son 2D (fond)
+
+        Debug.Log("🎵 Musique prête");
+    }
+
+    void Start()
+    {
+        if (musiquefondscene01 == null)
+        {
+            Debug.LogError("❌ AudioClip non assigné !");
+            return;
+        }
+
+        Debug.Log("▶️ Musique de fond lancée");
+        audioSource.Play();
+    }
+
+    void OnDestroy()
+    {
+        if (audioSource != null && audioSource.isPlaying)
+        {
+            Debug.Log("⛔ Arrêt musique (scene quit)");
+            audioSource.Stop();
+        }
+    }
+}
