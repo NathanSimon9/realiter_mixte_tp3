a6305e41abaaf81f351583dc073b97c75ae3bb63
Xav4737 <143350807+XavierMartineau@users.noreply.github.com>
2026-05-11 14:56:07 -0400

    Correction lava death

--- tp3_nathan_vasile_xavier/Assets/scripts/LavaDeath.cs
+++ tp3_nathan_vasile_xavier/Assets/scripts/LavaDeath.cs
@@ -1,11 +1,11 @@
- using UnityEngine;
+using UnityEngine;
 
 using UnityEngine.SceneManagement;
 
 using UnityEngine.UI;
 
 using System.Collections;
- 
+
 public class VRLavaDeath : MonoBehaviour
 
 {
